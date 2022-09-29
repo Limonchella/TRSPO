a597e5c36c8bac3161d41df23dab2b81241ca21e
Vantik Oleksii КІ-02 <63097646+Limonchella@users.noreply.github.com>
2022-09-29 11:58:25 +0300

    Update hometask2.py

--- hometask2.py
+++ hometask2.py
@@ -19,21 +19,39 @@ first_obj = Value1()
 second_obj = Value2()
 mutex1 = threading.Lock()
 mutex2 = threading.Lock()
-def increase(K):
-    mutex1.acquire()
-    for i in range(K):
-        first_obj.increase(random.randrange(1, 500)/10)
-    mutex1.release()
+def increase2(K):
     mutex2.acquire()
-    for i in range(K):
-        second_obj.increase(random.randrange(1, 500) / 10)
-    mutex2.release()
+    try:
+        for i in range(K):
+            second_obj.increase(random.randrange(1, 500)/10)
+    finally:
+        mutex2.release()
+    mutex1.acquire()
+    try:
+        for i in range(K):
+            first_obj.increase(random.randrange(1, 500) / 10)
+    finally:
+        mutex1.release()
+def increase1(K):
+    mutex1.acquire()
+    try:
+        for i in range(K):
+            first_obj.increase(random.randrange(1, 500)/10)
+    finally:
+        mutex1.release()
+    mutex2.acquire()
+    try:
+        for i in range(K):
+            second_obj.increase(random.randrange(1, 500) / 10)
+    finally:
+        mutex2.release()
+
 count = random.randint(10,20)
 threads = []
 for i in range(count//2):
-    threads.append(threading.Thread(target = increase, args = (random.randint(10000, 20000),)))
+    threads.append(threading.Thread(target = increase1, args = (random.randint(10000, 20000),)))
 for i in range(count//2):
-    threads.append(threading.Thread(target = increase, args = (random.randint(10000, 20000),)))
+    threads.append(threading.Thread(target = increase2, args = (random.randint(10000, 20000),)))
 start = time.time()
 for thread in threads:
     thread.start()
